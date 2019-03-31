4626247740ac04ddc75af2057dfbf7ca35a9f83c
rafbenz <rafbenz@gmail.com>
2019-03-31 18:16:32 -0400

    t

--- users/user.model.js
+++ users/user.model.js
@@ -8,7 +8,7 @@ const schema = new Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    verified: { type: Boolean, default: false },
+ 
     createdDate: { type: Date, default: Date.now }
 });
 

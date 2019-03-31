f9a1375ca09e56d301d7d82f560a63c0666b1d32
rafbenz <rafbenz@gmail.com>
2019-03-31 18:04:19 -0400

    fix verify

--- users/user.model.js
+++ users/user.model.js
@@ -7,6 +7,8 @@ const schema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    verified: { type: Boolean,default: false, required: false },
     createdDate: { type: Date, default: Date.now }
 });
 
